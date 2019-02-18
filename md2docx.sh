330303b0ebb4876528bc86fc0280bb16a4c3d657
shouldsee <shouldsee@github.com>
2019-02-18 16:23:28 +0000

    hard link script

---
+++ md2docx.sh
@@ -0,0 +1,19 @@
+clean(){
+sed -e 's/\(^\|[^*]\)\(\t\|* \|1\. \)//g' $1
+}
+
+main()
+{
+IN=${1:-MandM_BrachyCompMethod.md}
+ALI=${IN%.*}
+OPT="--filter pandoc-mustache --filter pandoc-citeproc"
+#cat $ALI.md | 
+clean $ALI.md | pandoc $OPT -f markdown -o $ALI.direct.docx
+
+clean $ALI.md | pandoc $OPT --webtex -f markdown -t html | tee $ALI.html \
+| pandoc -f html -o $ALI.docx
+
+
+
+}
+main "$@"
